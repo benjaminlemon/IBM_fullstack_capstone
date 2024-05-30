9c6738cc0ca7af3917d0274b9577617468ad8eee
benjaminlemon <benjamin.lemon1@gmail.com>
2024-05-30 12:11:34 -0600

    update djangoproj/settings

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://benjaminlem1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://benjaminlem1-8000.\
+                    theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.\
+                        cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://benjaminlem1-8000.theiadockernext-\
  1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
