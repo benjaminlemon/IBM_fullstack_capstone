a9bda79bfa153f85a6f64ba3233a3ea63f88ed55
benjaminlemon <73265828+benjaminlemon@users.noreply.github.com>
2024-05-30 10:45:03 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -28,6 +28,6 @@ class CarModel(models.Model):
                                    MaxValueValidator(2024),
                                    MinValueValidator(2015)
                                ])
-    
+
     def __str__(self):
         return self.name
